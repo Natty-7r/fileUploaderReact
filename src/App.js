56ff49985eb817c6952d97c843733b04f44e1ce3
natty-7 <nati7fekadu@gmail.com>
2023-01-18 01:43:05 +0300

    adding admin and store coordinator page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import Admin from "./components/pages/admin";
-function App() {
+import { useState } from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  redirect,
+  Routes,
+  Navigate,
+  Navigator,
+  useNavigate,
+} from "react-router-dom";
+
+import Header from "./components/adminComponents/header";
+import Admin from "./pages/admin";
+import Signup from "./pages/signup.";
+import Login from "./pages/login.js";
+import Coordinator from "./pages/coordinator";
+
+const storeDrugs = {
+  drugs: [1, 2, 3, 4, 5, 6, 7, 8],
+};
+function App(props) {
+  const loginHandler = () => {
+    console.log("login clicked");
+  };
   return (
-    <div className="App">
-      <Admin />
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        {" "}
+        <Route
+          path="/"
+          element={
+            <Coordinator
+              storeDrugs={storeDrugs}
+              loginHandler={loginHandler}
+            />
+          }
+        />
+        <Route
+          path="/"
+          element={<Admin />}
+        />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
