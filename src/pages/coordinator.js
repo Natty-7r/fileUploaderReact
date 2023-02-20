27b5f6ae2dab1facb37b7739d5d31af80eb34523
natty-7 <nati7fekadu@gmail.com>
2023-02-20 14:34:50 +0300

    working on login logic

--- src/pages/coordinator.js
+++ src/pages/coordinator.js
@@ -862,7 +862,7 @@ export default (props) => {
   return (
     <div className="whole_page coordinator_page">
       <Dashboard
-        username={username}
+        username={props.username}
         user="coordinator"
         notificationNum={notificationNum}
         currentSlide={currentSlide}
